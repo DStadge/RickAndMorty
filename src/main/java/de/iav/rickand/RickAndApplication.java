b148416b63e1433b826159db98595f9c01879d92
Dirk Stadge <dirk_stadge@yahoo.de>
2023-06-02 11:40:18 +0200

    inital Commit

---
+++ src/main/java/de/iav/rickand/RickAndApplication.java
@@ -0,0 +1,13 @@
+package de.iav.rickand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndApplication.class, args);
+    }
+
+}
